9fe968c175f872499154e999f660a19167583963
Harsh Chhabra <chhabraharsh37@gmail.com>
2022-03-24 13:26:41 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (96.50%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -2,11 +2,19 @@ class Solution {
 public:
     int fib(int n) {
         
-        if(n==0)
-            return 0;
-        if (n==1)
-            return 1;
+        if(n<2)
+            return n;
         
-        return fib(n-1)+fib(n-2);
+        int f,f0=0,f1=1;
+        
+        
+        for(int i=2;i<=n;i++)
+        {
+            f=f0+f1;
+            f0=f1;
+            f1=f;
+        }
+        
+        return f;
     }
 };
